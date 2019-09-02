1a4f2303649688ae262366996ddceb25b132698e
ema0898 <aguisnchez@gmail.com>
2019-09-02 11:09:18 -0600

    Agrega pruebas al procesador, falta probar branches

--- Proyecto1/src/Memories/instruction_memory.sv
+++ Proyecto1/src/Memories/instruction_memory.sv
@@ -19,6 +19,10 @@ module instruction_memory(input logic [21:0] a,
   assign memory[6] = 22'b1011110000100110010000; // LOAD R3, [R1, R2]
   assign memory[7] = 22'b1011111000101000000111; // LOAD R4, [R1, #7]
   assign memory[8] = 22'b1000000001101010100000; // SUM R5, R4, R3  
+  assign memory[9] = 22'b1000001010101010001111; // SUM R5, R5, #15
+  assign memory[10] = 22'b1001000010101100001000;  // MUL R6, R5, R1
+  assign memory[11] = 22'b1001001010001110000011; //MUL R7, R4, #3
+  assign memory[12] = 22'b1000111011100000110011; // CMP R7, #51
 	 
   assign rd = memory[a[21:2]]; // word aligned
 			  
